a129a9b124b68081a5e9aae79a7c4c08ab065970
rdiego26 <rdieg26@gmail.com>
2020-04-25 03:56:46 -0300

    ADD initial struct

---
+++ src/test/kotlin/me/diegoramos/pontointeligente/PontointeligenteApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.diegoramos.pontointeligente
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PontointeligenteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
